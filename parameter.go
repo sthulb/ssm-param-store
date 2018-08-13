c9744a0d266afbe2bf1b14b2d4215717ce3c9041
Simon Thulbourn <simon+github@thulbourn.com>
2018-08-13 18:46:56 +0100

    parameters

---
+++ parameter.go
@@ -0,0 +1,116 @@
+package store
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+var (
+	// ErrExpiredParameter thrown when the value of a param has expired
+	ErrExpiredParameter = errors.New("Parameter expired")
+
+	// ErrNoRefreshFn for when the parameter has no refresh func, but is asked to refresh
+	ErrNoRefreshFn = errors.New("No refresh function supplied")
+)
+
+// RefreshFn creates a callback for when a param value needs to be refreshed
+type RefreshFn func() (interface{}, error)
+
+// ParameterOptionFn type allows options to be specified for the parameter, such as auto refresh or expiry
+type ParameterOptionFn func(p *Parameter)
+
+// ValueExpires tells a parameter to refresh based on an expiry duration
+func ValueExpires(expiryTime time.Duration) ParameterOptionFn {
+	return func(p *Parameter) {
+		p.Expires = true
+		p.Expiry = expiryTime
+	}
+}
+
+// AutoRefreshValue will allow a parameter value to automatically refresh
+func AutoRefreshValue(autoRefresh bool) ParameterOptionFn {
+	return func(p *Parameter) {
+		p.AutoRefresh = autoRefresh
+		p.lastRefresh = time.Now()
+	}
+}
+
+// Parameter data structure
+type Parameter struct {
+	Name  string
+	Value interface{}
+
+	Expires bool
+	Expiry  time.Duration
+
+	AutoRefresh bool
+	RefreshFn   RefreshFn
+
+	lastRefresh time.Time
+}
+
+// NewParameter creates a new parameter based on options
+func NewParameter(name string, value interface{}, opts ...ParameterOptionFn) *Parameter {
+	param := &Parameter{
+		Expires: false,
+
+		Name:  name,
+		Value: value,
+	}
+
+	for _, o := range opts {
+		o(param)
+	}
+
+	return param
+}
+
+func (p *Parameter) StringValue() (string, error) {
+	if p.Expires {
+		return "", p.RefreshValue()
+	}
+
+	value := p.Value.(string)
+
+	return value, nil
+}
+
+func (p *Parameter) StringListValue() ([]string, error) {
+	if p.Expires {
+		return nil, p.RefreshValue()
+	}
+
+	value := p.Value.([]string)
+
+	return value, nil
+}
+
+// RefreshValue checks if a value is expired and refreshes it if required
+func (p *Parameter) RefreshValue() error {
+	lastSet := p.lastRefresh
+	expiry := p.Expiry
+
+	trueExpiry := lastSet.Add(expiry)
+	if trueExpiry.After(time.Now()) {
+		return nil
+	}
+
+	fmt.Printf("refreshfn: %v", p.RefreshFn)
+	if p.AutoRefresh && p.RefreshFn == nil {
+		return ErrNoRefreshFn
+	}
+
+	if p.AutoRefresh && p.RefreshFn != nil {
+		_, err := p.RefreshFn()
+		if err != nil {
+			return err
+		}
+
+		p.lastRefresh = time.Now()
+
+		return nil
+	}
+
+	return ErrExpiredParameter
+}
---
+++ parameter_test.go
@@ -0,0 +1,112 @@
+package store
+
+import (
+	"testing"
+	"time"
+)
+
+func TestParameter(t *testing.T) {
+	// t.Run("New Parameter", func(t *testing.T) {
+	// 	p := NewParameter("foo", nil)
+	// 	if p == nil {
+	// 		t.Fatalf("Unable to create a new parameter")
+	// 	}
+
+	// 	if fmt.Sprintf("%T", p) != "*store.Parameter" {
+	// 		t.Fatalf("Returned the wrong type: %v", fmt.Sprintf("%T", p))
+	// 	}
+	// })
+
+	// t.Run("New Parameter With Opts", func(t *testing.T) {
+	// 	expiry := time.Second * 10
+
+	// 	p := NewParameter("foo", nil, ValueExpires(expiry))
+
+	// 	if p.Expires != true || expiry != p.Expiry {
+	// 		t.Fatalf("Expiry time isn't correct: %v", p.Expires)
+	// 	}
+	// })
+
+	// t.Run("Refresh Parameter", func(t *testing.T) {
+	// 	expiry := time.Second * 1
+
+	// 	p := NewParameter("foo", nil, ValueExpires(expiry), AutoRefreshValue(true))
+
+	// 	if p.Expires != true || expiry != p.Expiry {
+	// 		t.Fatalf("Expiry time isn't correct: %v", p.Expires)
+	// 	}
+	// })
+
+	// t.Run("String Value", func(t *testing.T) {
+	// 	value := "bar"
+	// 	p := NewParameter("foo", value)
+
+	// 	retValue, _ := p.StringValue()
+	// 	if retValue != value {
+	// 		t.Fatalf("Error getting the value")
+	// 	}
+	// })
+
+	// t.Run("String Array Value", func(t *testing.T) {
+	// 	value := []string{"foo", "bar"}
+	// 	p := NewParameter("foo", value)
+
+	// 	retValue, _ := p.StringListValue()
+	// 	if !reflect.DeepEqual(retValue, value) {
+	// 		t.Fatalf("Error getting the value")
+	// 	}
+	// })
+
+	t.Run("Check Expiry in Future", func(t *testing.T) {
+		now := time.Now()
+		future := now.Add(time.Hour)
+
+		p := NewParameter("foo", "test")
+		p.Expires = true
+		p.lastRefresh = future
+
+		if err := p.RefreshValue(); err != nil {
+			t.Fatal("Dates in the future should not error")
+		}
+	})
+
+	t.Run("Check Expiry in Past", func(t *testing.T) {
+		past, _ := time.Parse("Mon, 01/02/06, 03:04PM", "Thu, 05/19/11, 10:47PM")
+
+		p := NewParameter("foo", "test")
+		p.Expires = true
+		p.lastRefresh = past
+
+		if err := p.RefreshValue(); err == nil {
+			t.Fatal("Dates in the past should error")
+		}
+	})
+
+	t.Run("Check Autorefresh enabled, but with no func", func(t *testing.T) {
+		past, _ := time.Parse("Mon, 01/02/06, 03:04PM", "Thu, 05/19/11, 10:47PM")
+
+		p := NewParameter("foo", "test", AutoRefreshValue(true))
+		p.Expires = true
+		p.lastRefresh = past
+
+		if err := p.RefreshValue(); err != ErrNoRefreshFn {
+			t.Fatalf("Parameters that have no refresh function should fail when auto refresh is enabled: %v", err)
+		}
+	})
+
+	t.Run("Check Autorefresh enabled, with func", func(t *testing.T) {
+		past, _ := time.Parse("Mon, 01/02/06, 03:04PM", "Thu, 05/19/11, 10:47PM")
+
+		p := NewParameter("foo", "test", AutoRefreshValue(true))
+		p.RefreshFn = func() (interface{}, error) {
+			return nil, nil
+		}
+
+		p.Expires = true
+		p.lastRefresh = past
+
+		if err := p.RefreshValue(); err != nil {
+			t.Fatal("Parameters that have no refresh function should fail when auto refresh is enabled")
+		}
+	})
+}
